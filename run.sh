f79826692ee03d1985ba17d168a7bbfffd283c4a
Ubuntu <ubuntu@ip-172-31-22-249.eu-west-1.compute.internal>
2019-01-02 20:08:05 +0000

    adding modification route

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 docker stop soap-ui-mock
 docker rm soap-ui-mock
 
-docker run --name soap-ui-mock -p 8082:9090 -v /home/usuario/proyectos/mockSoapUI/entrypoint.sh:/entrypoint.sh --entrypoint=/entrypoint.sh -d  soap-ui-mock
+docker run --name soap-ui-mock -p 7070:9090 -v /webapps/redbee/soap-ui-mock/entrypoint.sh:/entrypoint.sh --entrypoint=/entrypoint.sh -d saascolon/soap-ui-mock
