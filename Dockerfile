da67f795af3b81e800903c27ac801d735f9ed288
Lucas Avila <avila.lucas@colonseguros.com.ar>
2019-06-03 10:35:49 -0300

    se vacia changelog

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,7 @@ RUN /bin/bash -c "source $HOME/.sdkman/bin/sdkman-init.sh"
 
 RUN /bin/bash -c "source $HOME/.bashrc"
 
-RUN /bin/bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && echo -n | sdk install java 8.0.191-oracle"
+RUN /bin/bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && echo -n | sdk install java 8.0.212-zulu"
 
 RUN /bin/bash -c "wget https://s3.amazonaws.com/downloads.eviware/soapuios/5.4.0/SoapUI-5.4.0-linux-bin.tar.gz"
 
@@ -18,7 +18,7 @@ RUN tar -xzf SoapUI-5.4.0-linux-bin.tar.gz -C /root/
 
 RUN ls /root/
 
-ADD project.xml /project.xml
+#ADD project.xml /project.xml
 
 ADD entrypoint.sh /entrypoint.sh
 
--- run.sh
+++ run.sh
@@ -3,4 +3,7 @@
 docker stop soap-ui-mock
 docker rm soap-ui-mock
 
-docker run --name soap-ui-mock -p 7070:9090 -v /webapps/redbee/soap-ui-mock/entrypoint.sh:/entrypoint.sh --entrypoint=/entrypoint.sh -d saascolon/soap-ui-mock
+#docker run --name soap-ui-mock -p 7070:9090 -v /webapps/redbee/soap-ui-mock/entrypoint.sh:/entrypoint.sh --entrypoint=/entrypoint.sh -d saascolon/soap-ui-mock
+#docker run --name soap-ui-mock -p 7070:9090 -v /home/lucas/proyectos/soap-ui-mock/project.xml:/project.xml	-v /home/lucas/proyectos/soap-ui-mock/entrypoint.sh:/entrypoint.sh --entrypoint=/entrypoint.sh -d saascolon/soap-ui-mock
+
+docker run --name soap-ui-mock -p 7070:9090 -v /webapps/redbee/soap-ui-mock/project.xml:/project.xml -v /webapps/redbee/soap-ui-mock/entrypoint.sh:/entrypoint.sh --entrypoint=/entrypoint.sh -d saascolon/soap-ui-mock
