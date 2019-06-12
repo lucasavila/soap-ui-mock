7ac457276dbc94357cb114025771998c95badeac
Lucas Avila <avila.lucas@colonseguros.com.ar>
2019-06-12 09:30:18 -0300

    cotizacion mock se cambia a puerto 8080

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ docker rm soap-ui-mock
 #docker run --name soap-ui-mock -p 7070:9090 -v /webapps/redbee/soap-ui-mock/entrypoint.sh:/entrypoint.sh --entrypoint=/entrypoint.sh -d saascolon/soap-ui-mock
 #docker run --name soap-ui-mock -p 7070:9090 -v /home/lucas/proyectos/soap-ui-mock/project.xml:/project.xml	-v /home/lucas/proyectos/soap-ui-mock/entrypoint.sh:/entrypoint.sh --entrypoint=/entrypoint.sh -d saascolon/soap-ui-mock
 
-docker run --name soap-ui-mock -p 7070:9090 -v /webapps/redbee/soap-ui-mock/project.xml:/project.xml -v /webapps/redbee/soap-ui-mock/entrypoint.sh:/entrypoint.sh --entrypoint=/entrypoint.sh -d saascolon/soap-ui-mock
+docker run --name soap-ui-mock -p 8080:9090 -v /webapps/redbee/soap-ui-mock/project.xml:/project.xml -v /webapps/redbee/soap-ui-mock/entrypoint.sh:/entrypoint.sh --entrypoint=/entrypoint.sh -d saascolon/soap-ui-mock
